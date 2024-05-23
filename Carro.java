5927eb89123899857f68ead7a06105bd9738c2c7
maria1888 <48070915+maria1888@users.noreply.github.com>
2024-05-22 22:15:01 -0600

    estructuras carro's

--- Carro.java
+++ Carro.java
@@ -1,5 +1,34 @@
 package TallerDosParadigmas;
 
-public class Carro {
+public abstract class Carro {
+
+    //atributos
+    protected int velocidad;
+    protected String color;
+
+    //metodos
+    public abstract void acelerar();
+    public abstract void frenar();
+    public abstract void disparar();
+    public abstract void actualizar();
+
+    //getters y setters
+    public int getVelocidad(){
+
+        return velocidad;
+    }
+    public void setVelocidad(int velocidad){
+
+        this.velocidad = velocidad;
+
+    }
+    public string getColor(){
+
+        return color;
+    }
+    public void setColor(string color){
+
+        this.color = color;
+    }
     
 }
--- CarroFactory.java
+++ CarroFactory.java
@@ -1,1 +1,14 @@
 package TallerDosParadigmas;
+public class CarroFactory extends PolePositionFactory{
+    public Carro crearCarro(){
+
+        return new CarroJugador(0, null);
+    }
+
+    public Poder crearPoder(){
+
+        //se crea el poder 
+
+        return null;
+    }
+}
